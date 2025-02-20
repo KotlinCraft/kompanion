5b644ce3c2c5d63021e4cdc4baed366dc866d198
Quinten De Swaef <quintendeswaef@gmail.com>
2025-02-20 16:42:08 +0100

    feat: Add dialog support for chat screen configuration

--- src/main/kotlin/ChatScreen.kt
+++ src/main/kotlin/ChatScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import config.AppConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
