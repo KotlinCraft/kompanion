e23ffab13dcf65c2fff6b49b967095fb2901bfd4
Quinten De Swaef <quintendeswaef@gmail.com>
2025-02-24 12:05:30 +0100

    feat: Implement KompanionFileHandler for managing Kompanion directory and files

---
+++ src/main/kotlin/agent/fileops/KompanionFileHandler.kt
@@ -0,0 +1,68 @@
+package agent.fileops
+
+import config.AppConfig
+import java.io.File
+import java.io.IOException
+
+private const val KOMPANION_FOLDER = ".kompanion"
+
+class KompanionFileHandler {
+    companion object {
+        enum class KompanionFile(
+            val fileName: String
+        ) {
+            MESSAGE_HISTORY("message_history.txt"),
+        }
+
+        fun folderExists(): Boolean {
+            val latestDirectory = AppConfig.load().latestDirectory
+            val folder = File("$latestDirectory/$KOMPANION_FOLDER")
+            return folder.exists()
+        }
+
+        fun createFolder() {
+            val latestDirectory = AppConfig.load().latestDirectory
+            val folder = File("$latestDirectory/$KOMPANION_FOLDER")
+            if (!folder.exists()) {
+                folder.mkdirs()
+            }
+        }
+
+        fun readFromKompanionDirectory(fileName: String): String {
+            val latestDirectory = AppConfig.load().latestDirectory
+            val filePath = "$latestDirectory/$KOMPANION_FOLDER/$fileName"
+            return try {
+                val file = File(filePath)
+                file.readText()
+            } catch (e: IOException) {
+                println("Error reading file: ${e.message}")
+                ""
+            }
+        }
+
+        fun appendToKompanionFile(fileName: String, text: String) {
+            val latestDirectory = AppConfig.load().latestDirectory
+            val filePath = "$latestDirectory/$KOMPANION_FOLDER/$fileName"
+            try {
+                val file = File(filePath)
+                file.appendText(text)
+            } catch (e: IOException) {
+                println("Error appending to file: ${e.message}")
+
+            }
+        }
+
+        fun createFile(kompanionFile: KompanionFile) {
+            val latestDirectory = AppConfig.load().latestDirectory
+            val filePath = "$latestDirectory/$KOMPANION_FOLDER/${kompanionFile.fileName}"
+            try {
+                val file = File(filePath)
+                if (!file.exists()) {
+                    file.createNewFile()
+                }
+            } catch (e: IOException) {
+                println("Error creating file: ${e.message}")
+            }
+        }
+    }
+}
